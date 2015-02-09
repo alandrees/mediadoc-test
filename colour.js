4ddaea5caf67ea4e81c0f8b76fbbb11c34052571
Alan Drees <alandrees@theselves.com>
2015-02-09 10:11:23 -0500

    added the colour.js file with a namespace definition and file documentation header

---
+++ colour.js
@@ -0,0 +1,12 @@
+/**
+ * Copyright 2015 Alan Drees
+ *
+ * Purpose:
+ *   Javascript implementation for the mediadoc test application
+ *
+ * Dependencies:
+ *   AD ajax lib
+ */
+
+var MDT = MDT || {};
+
