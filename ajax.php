b526dbcb0f40f7757f8c8b7888db18b78699b19c
Alan Drees <alandrees@theselves.com>
2015-02-09 10:03:27 -0500

    added ajax.php with file documentation header

---
+++ ajax.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Copyright 2015 Alan Drees
+ *
+ * Purpose:
+ *   AJAX backend for accessing the database layer
+ *
+ * Requires:
+ *   /config.php
+ *   /lib.php
+ *
+ * Dependencies:
+ *
+ */
+
+?>
