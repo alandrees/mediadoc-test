4deb986a3fbdff1616d81c9f6a3b1540203dfd38
Alan Drees <alandrees@theselves.com>
2015-02-09 12:04:17 -0500

    added script.js with namespace and documentation header

---
+++ script.js
@@ -0,0 +1,22 @@
+/**
+ * Copyright 2015 Alan Drees
+ *
+ * Purpose:
+ *   Javascript implementation for the mediadoc test application
+ *
+ * Dependencies:
+ *   jQuery
+ *   jQueryUI
+ *   AD ajax lib
+ */
+
+var MDT = MDT || {};
+
+if(typeof MDT !== 'undefined')
+{
+    MDT.colours = {};
+
+    MDT.target = "votes-output";
+}
+
+
