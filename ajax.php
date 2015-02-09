d4e05ea936de743473a8c05c103410641fdf3729
Alan Drees <alandrees@theselves.com>
2015-02-09 10:04:39 -0500

    added input variable santitization

--- ajax.php
+++ ajax.php
@@ -17,5 +17,17 @@ require_once(__DIR__.'/config.php');
 require_once(__DIR__.'/lib.php');
 require_once(__DIR__.'/ajax_functions.php');
 
+/****************/
+/* Sanitization */
+/****************/
+
+array_walk_recursive($_GET, function(&$val, $index){ $val = strip_tags($val); });
+
+array_walk_recursive($_POST, function(&$val, $index){ $val = strip_tags($val); });
+
+array_walk_recursive($_COOKIE, function(&$val, $index){ $val = strip_tags($val); });
+
+array_walk_recursive($_REQUEST, function(&$val, $index){ $val = strip_tags($val); });
+
 
 ?>
