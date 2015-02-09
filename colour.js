8cee65e1ddc178cb1f6793f0ffb298c116de2e33
Alan Drees <alandrees@theselves.com>
2015-02-09 10:12:26 -0500

    added an object constructor for the MDT.Colour object

--- colour.js
+++ colour.js
@@ -10,3 +10,28 @@
 
 var MDT = MDT || {};
 
+/**\fn MDT.Colour
+ *
+ * Object constructor for the Colour object
+ *
+ * @param name (string) colour name
+ * @param renderer (function) renderer function
+ *
+ * Object representing a colour and it's data
+ */
+
+MDT.Colour = function(name, renderer)
+{
+    this._colour_name = name;
+    this._votes = [];
+    this._votes_retrieved = false;
+
+    if(typeof renderer !== 'function')
+    {
+	this.renderer = function(){console.log("Renderer not Assigned.  Nothing rendered.");}
+    }
+    else
+    {
+	this.renderer = renderer;
+    }
+}
