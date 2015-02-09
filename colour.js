a8d6d21b0dbb5684d362159b8d9a4179b6c41916
Alan Drees <alandrees@theselves.com>
2015-02-09 10:13:15 -0500

    added the MDT.Colour.prototype.sum function

--- colour.js
+++ colour.js
@@ -35,3 +35,30 @@ MDT.Colour = function(name, renderer)
 	this.renderer = renderer;
     }
 }
+
+
+/**\fn MDT.Colour.prototype.sum
+ *
+ * Sum the color's votes
+ *
+ * @param None
+ *
+ * @returns (integer) summation of the votes from each city for the colour
+ */
+
+MDT.Colour.prototype.sum = function()
+{
+    var votes = 0;
+
+    if(this._votes_retrieved === false)
+    {
+	this._get_votes();
+    }
+
+    for(var x = 0; x < this._votes.length; x++)
+    {
+	votes += this._votes[x].votes;
+    }
+
+    return votes;
+}
