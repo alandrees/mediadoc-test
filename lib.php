d11164975a65dcfc9f648bf54c16f27d95d3973c
Alan Drees <alandrees@theselves.com>
2015-02-09 10:28:06 -0500

    added lib.php to store all library functions

---
+++ lib.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Copyright 2015 Alan Drees
+ *
+ * Purpose:
+ *   Library functions for handling the acquisition of data from the database
+ *
+ * Requires:
+ *   /config.php
+ *
+ * Dependencies:
+ *
+ */
+
+?>
